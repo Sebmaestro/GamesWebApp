a9f79162b407ec9e72e4cd534bbbd198cc560b03
sebbe <sebbe@DESKTOP-DHCGTGB>
2023-04-08 14:11:54 +0200

    Add project files.

---
+++ GamesWebApp/Controllers/GamesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GamesWebApp.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Yatzy()
+        {
+            return View();
+        }
+
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ GamesWebApp/Models/Dice.cs
@@ -0,0 +1,15 @@
+﻿namespace GamesWebApp.Models
+{
+    public class Dice
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+
+        public int Roll { get; set; }
+
+        public Dice(int id)
+        {
+            this.Id = id;
+        }
+    }
+}
